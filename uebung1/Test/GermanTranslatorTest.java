423f7b99e481d0121b6fce0694899182eb4cccea
Fabius <gideon-schmitz@outlook.com>
2022-10-05 13:58:40 +0200

    Änderungen: Überarbeiter Test mittels Äquivalenzklassen

--- uebung1/Test/GermanTranslatorTest.java
+++ uebung1/Test/GermanTranslatorTest.java
@@ -4,10 +4,12 @@ import org.hbrs.s1.ws22.uebung1.control.Communication;
 import org.junit.jupiter.api.Assertions;
 
 class GermanTranslatorTest {
-    String fehler;
+    String fehler(int fehlerZahl){
+     return "Uebersetzung der Zahl " + fehlerZahl + " nicht moeglich, Interface Version 1.0";
+    }
 
-    int numArr[] = {-3, -2, -1, 0, 1, 3, 5, 7, 9, 10, 11, 467};
-    String ergbArr[] = {"Uebersetzung der Zahl [" + -3 + "] nicht möglich " + 1.0, "Uebersetzung der Zahl [" + -2 + "] nicht möglich " + 1.0, "Uebersetzung der Zahl [" + -1+ "] nicht möglich " + 1.0 , "Uebersetzung der Zahl [" + 0 + "] nicht möglich " + 1.0,"eins", "drei", "fuenf", "sieben", "neun", "zehn", "Uebersetzung der Zahl [" + 11 + "] nicht möglich " + 1.0, "Uebersetzung der Zahl [" + 467 + "] nicht möglich " + 1.0};
+    int numArr[] = {-5, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 200};
+    String ergbArr[] = {fehler(-5),  fehler(0),"eins", "zwei", "drei", "vier" ,"fuenf","sechs", "sieben", "acht", "neun", "zehn", fehler(200)};
 
     int i = 0;
     @org.junit.jupiter.api.Test
