aa039c089d11d62bc93ea0f0c30de6dc7faff79d
Haencky <115699809+Haencky@users.noreply.github.com>
2022-10-18 17:12:58 +0200

    Add files via upload

---
+++ uebung2/ContainerTest.java
@@ -0,0 +1,77 @@
+package org.hbrs.se1.ws22.uebung2.Test;
+
+import org.hbrs.se1.ws22.uebung2.ConcreteMember;
+import org.hbrs.se1.ws22.uebung2.Container;
+import org.hbrs.se1.ws22.uebung2.ContainerException;
+import org.junit.jupiter.api.*;
+
+/**
+ * Testklasse für die verschiedenen Fälle
+ */
+public class ContainerTest {
+    static Container c = new Container();   //Testcontainer
+
+    /**
+     * befüllt den Container
+     */
+    @BeforeEach
+    public void set() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                c.addMember(new ConcreteMember(i));
+            } catch (ContainerException e) {
+                e.toString();
+            }
+        }
+    }
+
+    /**
+     * Container wird ein Member hinzugefügt, der eine ID hat, der noch nicht im Container ist
+     * Erwartet, dass die Size von Container um eins erhöht wird
+     */
+    @Test
+    public void test1 () {
+        try {
+            c.addMember(new ConcreteMember(42));
+        } catch (ContainerException ce) {
+            ce.toString();
+        }
+        Assertions.assertEquals(6,c.size());
+        //c1.dump();
+    }
+
+    /**
+     * Entfernt ein Objekt aus dem Container, dass nicht vorhanden ist, oder fügt ein vorhandenes Objekt hinzu
+     * Erwartet, dass die Größe sich nicht verändert
+     */
+    @Test
+    public void test2() {
+        Assertions.assertEquals("Kein Member mit der ID 7 im Container vorhanden",c.deleteMember(7));
+        Assertions.assertEquals(5,c.size());
+        try {
+            c.addMember(new ConcreteMember(3));
+        } catch (ContainerException e) {
+            System.out.println(e.getMessage());
+        }
+        Assertions.assertEquals(5,c.size());
+    }
+
+
+    /**
+     * löscht ein Objekt aus dem Container, dass vorhanden ist
+     * Erwartet, dass die Size sich um eins verringert
+     */
+    @Test
+    public void test3 () {
+        Assertions.assertEquals("Member (ID = 2)",c.deleteMember(2));
+        Assertions.assertEquals(4,c.size());
+    }
+
+    /**
+     * resettet den Container für den nächsten Test
+     */
+    @AfterEach
+    public void reset() {
+        c = new Container();
+    }
+}
