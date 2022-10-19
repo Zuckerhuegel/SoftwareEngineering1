99f82f1c3762a49a4bc732f745517121e006d7f5
Fabius <gideon-schmitz@outlook.com>
2022-10-19 16:26:04 +0200

    Vorlage für Uebung 3

---
+++ uebung3/ConcreteMember.java
@@ -0,0 +1,36 @@
+package org.hbrs.s1.ws22.uebung3;
+
+/**
+ * Konkreter Memberklasse, um das Interface Member nutzen zu können
+ */
+public class ConcreteMember implements Member{
+    private Integer id;     //id des Members
+
+    /**
+     * Gibt die ID des Members zurück
+     * @return ID des Members
+     */
+    @Override
+    public Integer getID() {
+        return id;
+    }
+
+    /**
+     * Gibt das Memberobjekt als String zurück
+     * @return String, der das Memberobjekt beschreibt
+     */
+    @Override
+    public String toString() {
+        return "Member (ID = " + id.toString() + ")";
+    }
+
+    /**
+     * Konstruktor für einen konkreten Memberobjekt
+     * @param id ID die der Member hat soll
+     */
+    public ConcreteMember (Integer id) {
+        this.id = id;
+    }
+
+
+}
